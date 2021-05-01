52ff1be147c49fcd394f2c8a141be5902cc73dd2
Jingu Kang <jinguk0816@gmail.com>
2021-05-01 14:15:06 -0400

    add basic route for all the menu items

---
+++ routes/menu.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/:id", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get()
+  return router;
+};
+
